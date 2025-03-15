227c5a51628f3b4279889d683342c16404b568e0
cheonhy25u <kor01chs@gmail.com>
2025-03-15 20:42:45 +0900

    chapter 01-1 입출력

---
+++ basic/helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //헤더파일 선언
+
+int main(void){
+    int num = 20;
+    std::cout<<"Hello World!"<<std::endl;
+    std::cout<<"Hello "<<"World!"<<std::endl;
+    std::cout<<num<<' '<<'A';
+    std::cout<<' '<<3.14<<std::endl;
+    return 0;
+}
